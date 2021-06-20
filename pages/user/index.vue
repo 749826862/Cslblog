6f7f63470172a66bd7ba376683dfb0f4f554e8ea
chenshuanglin <chenshuanglin@yunlizhihui.com>
2021-06-20 20:47:44 +0800

    路由跳转

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Cslblog
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-    <nuxt-link to="/user">文章</nuxt-link>
+    我的第一个nuxt应用
   </div>
 </template>
 
