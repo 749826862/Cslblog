5b9cd303f1b9f1174e13f01bbee0f719a1ecded8
chenshuanglin <chenshuanglin@yunlizhihui.com>
2021-06-27 21:04:00 +0800

    集成velocity

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,50 +1,26 @@
 <template>
-  <div class="container">
+  <div id="container">
     我的第一个nuxt应用
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted(){
+    let box = document.getElementById('container')
+    let x = this.$velocity(box, {
+      top: 500
+    })
+    console.log(x, 888)
+    this.$velocity(box, {left: 300})
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+#container {
+  width: 300px;
+  position: relative;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
