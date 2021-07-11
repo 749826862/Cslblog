aafce94c93f57ea68f839cf073250dbefd942048
chenshuanglin <chenshuanglin@yunlizhihui.com>
2021-07-11 20:58:18 +0800

    搭建架子

---
+++ components/edit-head/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="edit-head">
+      头部
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.edit-head{
+    background: red;
+}
+</style>
